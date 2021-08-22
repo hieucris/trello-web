2dfcc12d1d78521a0a72dcff07b6170b2ad4d3d3
Hieu Cris <hieuhk195@gmail.com>
2021-08-22 10:40:43 +0700

    Design Trello

--- src/App.js
+++ src/App.js
@@ -1,9 +1,136 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello world</p>
+    <div className="trello-app">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar broad">Board Bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstore</header>
+          <ul>
+            <li>
+              <img
+                src="https://image-us.24h.com.vn/upload/1-2019/images/2019-01-11/ronaldo-660-1547196192-632-width660height451.jpg"
+                alt="hieucris"
+              />
+              Title : HieuCris
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstore</header>
+          <ul>
+            <li>
+              <img
+                src="https://image-us.24h.com.vn/upload/1-2019/images/2019-01-11/ronaldo-660-1547196192-632-width660height451.jpg"
+                alt="hieucris"
+              />
+              Title : HieuCris
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstore</header>
+          <ul>
+            <li>
+              <img
+                src="https://image-us.24h.com.vn/upload/1-2019/images/2019-01-11/ronaldo-660-1547196192-632-width660height451.jpg"
+                alt="hieucris"
+              />
+              Title : HieuCris
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstore</header>
+          <ul>
+            <li>
+              <img
+                src="https://image-us.24h.com.vn/upload/1-2019/images/2019-01-11/ronaldo-660-1547196192-632-width660height451.jpg"
+                alt="hieucris"
+              />
+              Title : HieuCris
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstore</header>
+          <ul>
+            <li>
+              <img
+                src="https://image-us.24h.com.vn/upload/1-2019/images/2019-01-11/ronaldo-660-1547196192-632-width660height451.jpg"
+                alt="hieucris"
+              />
+              Title : HieuCris
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstore</header>
+          <ul>
+            <li>
+              <img
+                src="https://image-us.24h.com.vn/upload/1-2019/images/2019-01-11/ronaldo-660-1547196192-632-width660height451.jpg"
+                alt="hieucris"
+              />
+              Title : HieuCris
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstore</header>
+          <ul>
+            <li>
+              <img
+                src="https://image-us.24h.com.vn/upload/1-2019/images/2019-01-11/ronaldo-660-1547196192-632-width660height451.jpg"
+                alt="hieucris"
+              />
+              Title : HieuCris
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
