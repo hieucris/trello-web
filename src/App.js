a79380ef1cdd30e938fb034ba798f217ad4c8d6d
Hieu Cris <hieuhk195@gmail.com>
2021-08-23 16:52:04 +0700

    clean architecture

--- src/App.js
+++ src/App.js
@@ -1,136 +1,14 @@
+import AppBar from "components/AppBar/AppBar";
+import BoardBar from "components/BoardBar/BoardBar";
+import BoardContent from "components/BoardContent/BoardContent";
 import "./App.scss";
 
 function App() {
   return (
     <div className="trello-app">
-      <nav className="navbar app">App Bar</nav>
-      <nav className="navbar broad">Board Bar</nav>
-      <div className="board-columns">
-        <div className="column">
-          <header>Brainstore</header>
-          <ul>
-            <li>
-              <img
-                src="https://image-us.24h.com.vn/upload/1-2019/images/2019-01-11/ronaldo-660-1547196192-632-width660height451.jpg"
-                alt="hieucris"
-              />
-              Title : HieuCris
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstore</header>
-          <ul>
-            <li>
-              <img
-                src="https://image-us.24h.com.vn/upload/1-2019/images/2019-01-11/ronaldo-660-1547196192-632-width660height451.jpg"
-                alt="hieucris"
-              />
-              Title : HieuCris
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstore</header>
-          <ul>
-            <li>
-              <img
-                src="https://image-us.24h.com.vn/upload/1-2019/images/2019-01-11/ronaldo-660-1547196192-632-width660height451.jpg"
-                alt="hieucris"
-              />
-              Title : HieuCris
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstore</header>
-          <ul>
-            <li>
-              <img
-                src="https://image-us.24h.com.vn/upload/1-2019/images/2019-01-11/ronaldo-660-1547196192-632-width660height451.jpg"
-                alt="hieucris"
-              />
-              Title : HieuCris
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstore</header>
-          <ul>
-            <li>
-              <img
-                src="https://image-us.24h.com.vn/upload/1-2019/images/2019-01-11/ronaldo-660-1547196192-632-width660height451.jpg"
-                alt="hieucris"
-              />
-              Title : HieuCris
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstore</header>
-          <ul>
-            <li>
-              <img
-                src="https://image-us.24h.com.vn/upload/1-2019/images/2019-01-11/ronaldo-660-1547196192-632-width660height451.jpg"
-                alt="hieucris"
-              />
-              Title : HieuCris
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstore</header>
-          <ul>
-            <li>
-              <img
-                src="https://image-us.24h.com.vn/upload/1-2019/images/2019-01-11/ronaldo-660-1547196192-632-width660height451.jpg"
-                alt="hieucris"
-              />
-              Title : HieuCris
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-      </div>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
     </div>
   );
 }
---
+++ src/components/Task/Task.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Task.scss";
+
+function Task(props) {
+  return (
+    <li className="task-item">
+      <img
+        src="https://image-us.24h.com.vn/upload/1-2019/images/2019-01-11/ronaldo-660-1547196192-632-width660height451.jpg"
+        alt="hieucris"
+      />
+      Title : HieuCris
+    </li>
+  );
+}
+
+export default Task;
