3eaf2b0b1fe4a0f22704c69b2c7623fbc919c10c
Hieu Cris <hieuhk195@gmail.com>
2021-09-02 16:09:41 +0700

    implement drag and drop

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,12 +4,17 @@ import "./Card.scss";
 function Card(props) {
   const { card } = props;
   return (
-    <li className="card-item">
+    <div className="card-item">
       {card.cover && (
-        <img src={card.cover} className="card-cover" alt="card-img" />
+        <img
+          src={card.cover}
+          className="card-cover"
+          alt="card-img"
+          onMouseDown={(e) => e.preventDefault()}
+        />
       )}
       {card.title}
-    </li>
+    </div>
   );
 }
 
